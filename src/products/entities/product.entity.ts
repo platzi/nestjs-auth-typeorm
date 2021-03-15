bd5e5749355d9af464df693a5f145f41aa582267
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-15 13:18:03 -0500

    :construction: work

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
+
+  @Column({ type: 'text' })
   description: string;
+
+  @Column({ type: 'int' })
   price: number;
+
+  @Column({ type: 'int' })
   stock: number;
+
+  @Column({ type: 'varchar' })
   image: string;
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
+import { Product } from './entities/product.entity';
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
-import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
