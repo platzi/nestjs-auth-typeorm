b3dcca823b58f476e94a34d93f1ea8864bd51ff4
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-16 10:14:53 -0500

    :construction: work
    work

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,13 @@ export default registerAs('config', () => {
       user: process.env.POSTGRES_USER,
       host: process.env.POSTGRES_HOST,
     },
+    mysql: {
+      dbName: process.env.MYSQL_DATABASE,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      user: process.env.MYSQL_USER,
+      host: process.env.MYSQL_HOST,
+    },
     apiKey: process.env.API_KEY,
   };
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -19,9 +19,9 @@ const API_KEY_PROD = 'PROD1212121SA';
     TypeOrmModule.forRootAsync({
       inject: [config.KEY],
       useFactory: (configService: ConfigType<typeof config>) => {
-        const { user, host, dbName, password, port } = configService.postgres;
+        const { user, host, dbName, password, port } = configService.mysql;
         return {
-          type: 'postgres',
+          type: 'mysql',
           host,
           port,
           username: user,
