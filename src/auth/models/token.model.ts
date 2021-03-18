05d65347cc4e62a4341b528be4d6e220225a2319
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-18 14:12:21 -0500

    :construction: work

---
+++ src/auth/models/token.model.ts
@@ -0,0 +1,4 @@
+export interface PayloadToken {
+  role: string;
+  sub: number;
+}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from './../../users/services/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from './../../users/services/users.service';
+import { User } from './../../users/entities/user.entity';
+import { PayloadToken } from './../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.usersService.findByEmail(email);
@@ -15,5 +22,13 @@ export class AuthService {
       }
     }
     return null;
+  }
+
+  generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
