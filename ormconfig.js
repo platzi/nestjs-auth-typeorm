36b6d0e69a8faf0f1876095e13c245106be6879a
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-19 12:38:43 -0500

    save

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  logging: false,
+  migrations: ['src/database/migrations/*.ts'],
+  migrationsTableName: 'migrations',
+  entities: ['src/**/*.entity.ts'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+  },
+};
